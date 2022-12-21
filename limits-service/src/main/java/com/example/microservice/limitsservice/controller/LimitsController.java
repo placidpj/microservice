221884e2d8e125114c26bd36f847be338adf2891
Prashant Jakhar <placidpj@gmail.com>
2022-12-21 14:08:27 +0530

    Reading values from properties file.

--- limits-service/src/main/java/com/example/microservice/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/example/microservice/limitsservice/controller/LimitsController.java
@@ -1,15 +1,20 @@
 package com.example.microservice.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.microservice.limitsservice.bean.Limits;
+import com.example.microservice.limitsservice.configuration.Configuration;
 
 @RestController
 public class LimitsController {
 
+	@Autowired
+	private Configuration configuration;
+	
 	@GetMapping("/limits")
 	public Limits retrieveLimits() {
-		return new Limits(1, 1);
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
 	}
 }
