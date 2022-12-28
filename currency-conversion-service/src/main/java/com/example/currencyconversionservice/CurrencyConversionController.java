b6244a8f5220eaeb1df007add975a22b177b88e5
placidpj <106440023+placidpj@users.noreply.github.com>
2022-12-28 17:55:29 +0530

    Added Feign client

--- currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.example.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,9 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+	
+	@Autowired
+	private CurrencyExchangeProxy currencyExchangeProxy;
 	
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(
@@ -29,5 +33,19 @@ public class CurrencyConversionController {
 				currencyConversion.getConversionMultiple(),
 				quantity.multiply(currencyConversion.getConversionMultiple()),
 				currencyConversion.getEnvironment());
+	}
+	
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(
+			@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		
+		CurrencyConversion currencyConversion = currencyExchangeProxy.retrieveExchangeValue(from, to);
+		
+		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity, 
+				currencyConversion.getConversionMultiple(),
+				quantity.multiply(currencyConversion.getConversionMultiple()),
+				currencyConversion.getEnvironment() + " feign");
 	}
 }
--- currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+// Scans for interfaces that declare they are feign clients. 
+// And configures component scanning directives for use with @Configuration classes.
+@EnableFeignClients
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
