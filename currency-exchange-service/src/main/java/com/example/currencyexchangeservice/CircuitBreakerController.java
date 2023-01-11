4cd8ad9337016e6820b898f67f90babc10ffc173
placidpj <106440023+placidpj@users.noreply.github.com>
2023-01-11 22:41:57 +0530

    Added more Circuit breaker features.

--- currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CircuitBreakerController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -44,4 +47,29 @@ public class CircuitBreakerController {
 	public String hardcodedResponse(Exception ex) {
 		return "fallback-response";
 	}
+
+	
+//  Circuit breaker have 3 stages. Initially when application start it is in Closed state. If all the requests 
+//	are failing then it will go to Open state and then it will occasionally hit the endpoint i.e. that is it changed to half-open state
+//	to see if they are working, if yes, then status will change to Closed otherwise it will go to half-open 
+//	status where it occasionally hit the endpoint to check if the endpoint started working. 
+//	If circuit breaker goes to open state then the controll will  not go inside the below method i.e. will not pring
+//	the log statement. 
+	@GetMapping("/sample-api-circuit-breaker")
+//	@Retry(name = "sample-api-name", fallbackMethod = "hardcodedResponse")
+	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+	public String sampleApiCircuitBreaker() {
+		log.info("Sample api circuit breaker called");
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:1234/unavailable",
+				String.class);
+		return forEntity.getBody();
+	}
+
+//	Puts limit on the number of calls made per unit of time. eg.10 calls per minute
+//	@RateLimiter(name="default")
+	
+//	Number of concurrent calls that can be allowed.
+//	@Bulkhead(name="sample-api")
+	
+	
 }
