94e9189f0dd08b8effa17e835f6561fa74eaca85
placidpj <106440023+placidpj@users.noreply.github.com>
2023-01-12 23:15:33 +0530

    Print logs with traceID and span ID

--- currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.example.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	private Logger log = LoggerFactory.getLogger(getClass());
+	
 	@Autowired
 	private CurrencyExchangeRepository repository;
 	
@@ -18,6 +23,7 @@ public class CurrencyExchangeController {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		log.info("retrieveExchangeValue called with {} to {}", from, to);
 //		 CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 		if (currencyExchange == null) {
